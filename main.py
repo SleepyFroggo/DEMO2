2561eafa419d7c83675f7b96d35bdca055d35bb2
SleepyFroggo <reanier.james.vertudez@aclcbutuan.edu.ph>
2025-06-13 14:58:02 +0800

    Display Info

--- main.py
+++ main.py
@@ -7,5 +7,11 @@ app = Flask(__name__,template_folder='templates')
 def login():
     return render_template('login.html')
 
+# Route for profile page
+@app.route('/profile')
+def profile():
+    return "profile.html"
+
+
 if __name__ == '__main__':
     app.run(debug=True)
